9aafd32f11e38f1f9fa768cb14030a1cb31cfa03
andreacarrascor <ancarami19@gmail.com>
2021-08-31 16:26:06 -0400

    Eliminamos la pagina2 de html y creamos una funcion para llamarla desde js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+//declaramos las variables para obtener los elementos de pagina 1 y pagina 2
+const displayOne=
+    document.getElementById("page1");
+
+const displayTwo=
+    document.getElementById("page2");
+
+//declaramos una variable y función para mostrar la pagina 2, mediante un evento listener
+const showPage2=
+    document.getElementById("showpage2");
+    showPage2.addEventListener("click", () => {
+    displayOne.style.display= "none";
+    displayTwo.style.display= "block";
+})
