361a84be33eeb74aad7f155d9dca91715b3dba71
andreacarrascor <ancarami19@gmail.com>
2021-09-06 21:35:09 -0300

    cambios en html, css y despliegue segunda página durante sprint 2

--- src/cipher.js
+++ src/cipher.js
@@ -1,7 +1,31 @@
-const cipher = {
-  
+const cipher = () => {
+//const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const offset = document.getElementById("position");
+const input = document.getElementById("inputText");
+// const output = document.getElementById("outputText");
+const encodeButton = document.getElementById("encode");
+const decodeButton = document.getElementById("decode");
+  let result = "";
+    for (let i=0; i < input.lenght; i++) {
+      if(input.charCodeAt(i)>= 65 && input.charCodeAt(i)<=90) {
+        result+=String.fromCharCode((input.charCodeAt(i) - 65 + offset) % 26 + 65);
+      } result+=String.fromCharCode(input.charCodeAt(i));
 
-  // debe contener funciones encode y decode
-};
+      } document.getElementById("outputText").value = result;
+      
+      return result; 
+    } 
+
+  
+     
+
+
+
+//llamamos a la variable para obtener codigo ASCII de la letra seleccionada
+//alphabet.charCodeAt();
+//para convertir de código ASCII a letra 
+//Alphabet.fromCharCode();
+//fórmula cifrado César
+//(x-65 + n)% 26 + 65
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -2,29 +2,5 @@ import cipher from './cipher.js';
 
 console.log(cipher);
 
-//declaramos las variables para obtener los elementos de paginas 1, 2 y 3
-const displayOne=
-    document.getElementById("page1");
 
-const displayTwo=
-    document.getElementById("page2");
-
-const displayThree=
-    document.getElementById("instructions");
-
-//declaramos una variable y función para mostrar la pagina 2, mediante un evento listener
-const showPage2=
-    document.getElementById("showpage2");
-    showPage2.addEventListener("click", () => {
-    displayOne.style.display= "none";
-    displayTwo.style.display= "block";
-
-})
-const showInstructions=
-    document.getElementById("showinstructions");
-    showInstructions.addEventListener("click", () => {
-    displayOne.style.display= "none";
-    displayTwo.style.display= "none";
-    displayThree.style.display= "block";
-})
     
